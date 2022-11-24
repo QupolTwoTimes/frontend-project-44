07adc61abd64d0ae066e4811c2b73385efc0de29
Qupol Two Times <ilya.v-i-n-o@yandex.ru>
2022-11-24 22:12:28 +0300

    finish step 9

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import * as primeModule from '../src/games/prime.js';
+
+primeModule.makeStart();
---
+++ src/games/prime.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-expressions */
+import readlineSync from 'readline-sync';
+import {
+  welcome, askName, sayHello, compareResults, congratulations,
+} from '../index.js';
+
+welcome();
+const userName = askName();
+sayHello(userName);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+let trueAnswerCounter = 0;
+let checkValue = '';
+let userAnswer = '';
+
+do {
+  const randomNumber = Math.floor(Math.random() * (206 - 5) + 1);
+  for (let i = 2; i < randomNumber; i += 1) {
+    if (randomNumber === 1 || randomNumber === 2 || randomNumber === 3 || randomNumber === 5
+        || randomNumber === 7) {
+      checkValue = 'yes';
+      break;
+    } else if (randomNumber % i === 0) {
+      checkValue = 'no';
+      break;
+    } else {
+      checkValue = 'yes';
+    }
+  }
+  userAnswer = readlineSync.question(`Question: ${randomNumber} `);
+  compareResults(userAnswer, checkValue);
+  trueAnswerCounter += 1;
+} while (trueAnswerCounter !== 3 && userAnswer === checkValue);
+
+congratulations(trueAnswerCounter, userName, userAnswer, checkValue);
+
+const makeStart = () => {
+};
+
+export {
+  welcome, askName, sayHello, compareResults, congratulations, makeStart,
+};
