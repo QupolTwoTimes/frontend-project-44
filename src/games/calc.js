64ab1e5b7d3f079ef23ba097a9ec4ff18bf3936f
Qupol Two Times <ilya.v-i-n-o@yandex.ru>
2022-12-10 11:36:56 +0300

    fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,7 +7,7 @@ const gameRules = 'What is the result of the expression?';
 
 const getRandomOperator = () => {
   const operators = ['+', '-', '*'];
-  return operators[generateRandomInRange(0, operators.length + 1)];
+  return operators[generateRandomInRange(0, operators.length - 1)];
 };
 
 const calculate = (num1, num2, operator) => {
@@ -15,17 +15,14 @@ const calculate = (num1, num2, operator) => {
     return num1 + num2;
   } if (operator === '-') {
     return num1 - num2;
-  } if (operator === '*') {
-    return num1 * num2;
   }
-  return `throw new Error(Operator ${operator} is incorrect)`;
+  return num1 * num2;
 };
 
 const generateRound = () => {
   const num1 = generateRandomInRange(0, 10);
   const num2 = generateRandomInRange(0, 10);
   const operator = getRandomOperator();
-  console.log(operator);
   const question = readlineSync.question(`Question: ${num1} ${operator} ${num2} `);
   const answer = String(calculate(num1, num2, operator));
   return [question, answer];
